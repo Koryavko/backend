04327dfe329b41dea2575bd6c9d081a46cb08e9b
Fullstack_Denys_Z <denys.zaiarnyi@abz.agency>
2023-12-22 21:56:24 +0200

    feature: project init

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+import * as compression from 'compression';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  app.use(helmet());
+  app.use(compression());
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(
+    bodyParser.urlencoded({
+      limit: '50mb',
+      extended: true,
+      parameterLimit: 50000,
+    }),
+  );
+  app.use(
+    rateLimit({
+      windowMs: 1000 * 60 * 60,
+      max: 1000, // 1000 requests por windowMs
+      message: '⚠️  Too many request created from this IP, please try again after an hour',
+    }),
+  );
+
+  await app.listen(3000);
+}
+bootstrap();
